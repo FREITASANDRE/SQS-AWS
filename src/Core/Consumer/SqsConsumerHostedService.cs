183c4e8b91cfd10a5145e9cd1419975c1a7d434a
Andre Freitas <36485479+FREITASANDRE@users.noreply.github.com>
2024-01-30 15:56:16 -0300

    Update SqsConsumerHostedService.cs

--- src/Core/Consumer/SqsConsumerHostedService.cs
+++ src/Core/Consumer/SqsConsumerHostedService.cs
@@ -1,25 +1,83 @@
-﻿using MessageBroker.Sqs.Abstractions;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using MessageBroker.Sqs.Abstractions;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
 
 namespace MessageBroker.Sqs.Consumer
 {
-    public class SqsConsumerHostedService<TMessage> : IHostedService where TMessage : IMessage
+    public class SqsConsumerHostedService<TMessage> : BackgroundService where TMessage : IMessage
     {
-        private readonly ISqsConsumerService<TMessage> _consumerService;
+        private readonly ILogger<SqsConsumerHostedService<TMessage>> _logger;
+        private readonly IAmazonSQS _sqs;
+        private readonly MessageDispatcher _dispatcher;
+        private readonly string _queueName;
+        private readonly List<string> _messageAttributeNames = new() { "All" };
 
-        public SqsConsumerHostedService(ISqsConsumerService<TMessage> consumerService)
+        public SqsConsumerHostedService(ILogger<SqsConsumerHostedService<TMessage>> logger, IAmazonSQS sqs, MessageDispatcher dispatcher, string queueName)
         {
-            _consumerService = consumerService;
+            _logger = logger;
+            _sqs = sqs;
+            _dispatcher = dispatcher;
+            _queueName = queueName;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            return _consumerService.StartAsync(cancellationToken);
-        }
+            var listQueuesRequest = new ListQueuesRequest
+            {
+                QueueNamePrefix = _queueName
+            };
 
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            return Task.CompletedTask;
+            var response = await _sqs.ListQueuesAsync(listQueuesRequest);
+
+            if (response.QueueUrls.Count == 0)
+            {
+                _logger.LogError($"Queue {_queueName} does not exist");
+                return;
+            }
+
+            var queueUrl = await _sqs.GetQueueUrlAsync(_queueName, cancellationToken);
+            var receiveRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl.QueueUrl,
+                MessageAttributeNames = _messageAttributeNames,
+                AttributeNames = _messageAttributeNames
+            };
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var messageResponse = await _sqs.ReceiveMessageAsync(receiveRequest, cancellationToken);
+                if (messageResponse.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    continue;
+                }
+
+                foreach (var message in messageResponse.Messages)
+                {
+                    var messageTypeName = message.MessageAttributes
+                        .GetValueOrDefault(nameof(IMessage.MessageTypeName))?.StringValue;
+
+                    if (messageTypeName is null)
+                    {
+                        continue;
+                    }
+
+                    if (!_dispatcher.CanHandleMessageType(messageTypeName))
+                    {
+                        continue;
+                    }
+
+                    var messageType = _dispatcher.GetMessageTypeByName(messageTypeName)!;
+
+                    var messageAsType = (IMessage)JsonSerializer.Deserialize(message.Body, messageType)!;
+
+                    await _dispatcher.DispatchAsync(messageAsType);
+                    await _sqs.DeleteMessageAsync(queueUrl.QueueUrl, message.ReceiptHandle, cancellationToken);
+                }
+            }
         }
     }
 }
